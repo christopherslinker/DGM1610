42e2ccae7daa65dee94a4ed5958be22664660d5d
Christopher Slinker <christopher.slinker@gmail.com>
2021-01-31 19:55:20 -0700

    Loops

---
+++ SimpleScenePractice/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello world!");
+        } while (shouldContinue == true);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
