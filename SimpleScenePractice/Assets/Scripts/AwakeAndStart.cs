15378936e2d20e3ddcd2f0e3d3cbf3602dd48f4d
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-01 20:19:10 -0700

    Awake And Start

--- SimpleScenePractice/Assets/Scripts/AwakeAndStart.cs
+++ SimpleScenePractice/Assets/Scripts/AwakeAndStart.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class AwakeAndStart : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    void Awake()
+    {
+        Debug.Log("I am awake.");
+    }
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Start called.");
     }
 }
