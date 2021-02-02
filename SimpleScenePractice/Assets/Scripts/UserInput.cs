461c96056f5a1505c8752a538c90c679bf22c7f1
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-02 08:04:54 -0700

    Translate Movement
    Ball moves left right and floats up and down with arrow keys.

---
+++ SimpleScenePractice/Assets/Scripts/UserInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+
+    public Rigidbody2D ballRigidbody2D;
+    public Vector2 direction;
+    public Vector2 yDirection;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ballRigidbody2D.AddForce(-direction, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
+        }
+    }
+}
