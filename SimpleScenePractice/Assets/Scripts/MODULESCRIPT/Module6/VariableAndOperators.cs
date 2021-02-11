f61cbc7103144fcf04f09c413e415380bc827b32
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-10 19:16:59 -0700

    Variable and Operators
    Did variables and operators practice in module 6

--- SimpleScenePractice/Assets/Scripts/MODULESCRIPT/Module6/VariableAndOperators.cs
+++ SimpleScenePractice/Assets/Scripts/MODULESCRIPT/Module6/VariableAndOperators.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,18 @@ public class VariableAndOperators : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int myInt = 50;
+        string myName = "John";
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        int x = 5, y = 10, z = 50;
+        Console.WriteLine(x + y + z);
+        Console.WriteLine(z);
+        Console.WriteLine(x+y);
+        Console.WriteLine(myInt);
+        Console.WriteLine(myName);
+
     }
 
     // Update is called once per frame
