622a9614028995ad1d184e5498ae931562c8e810
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-14 21:39:36 -0700

    Counter Still Not Working
    I tried to get the counter to work but somehow ended up with an extra error. Made it worse.

--- SimpleScenePractice/Assets/Scripts/Counter.cs
+++ SimpleScenePractice/Assets/Scripts/Counter.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +7,12 @@ public class Counter : MonoBehaviour
     public int creditValue = 3;
     public IntData numberCount;
     public Text counterText;
+
+    private void Start()
+    {
+        counterText.text = numberCount.value.ToString();
+    }
+
     void OnTriggerEnter2D()
     {
         numberCount.value += creditValue;
