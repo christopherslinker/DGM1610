7a9fea6edbffd88b0a8cc7a3c42ab7239311140e
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-18 21:14:36 -0700

    For Loops
    Did a For Loop practice

---
+++ SimpleScenePractice/Assets/Scripts/ForLoopsWithArrays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class ForLoopsWithArrays : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 10; i >= 0; i++)
+            Debug.Log(i);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
