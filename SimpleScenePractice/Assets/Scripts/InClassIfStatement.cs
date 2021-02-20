facbbd9c82888c5291b857d3bceae2034c1d71e1
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-19 18:43:45 -0700

    More If Statements

---
+++ SimpleScenePractice/Assets/Scripts/InClassIfStatement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InClassIfStatement : MonoBehaviour
+
+{
+    public int a, b, c;
+    public string password;
+    public bool canEnter;
+
+    public enum GameStates
+    {
+        Starting, Playing, Ending
+    }
+
+    public GameStates currentGameState;
+
+    void Start()
+    {
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                print("Starting");
+                break;
+            case GameStates.Playing:
+                print("Playing");
+                break;
+            case GameStates.Ending:
+                print("Ending");
+                break;
+        }
+
+        if (a + b == c)
+        {
+            print(true);
+            
+        }
+        else
+        {
+            print(false);
+        }
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
