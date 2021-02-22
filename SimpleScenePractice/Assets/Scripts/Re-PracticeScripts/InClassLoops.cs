840bd26317bc16f0f23e79701e4e7873f8efb561
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-22 10:56:42 -0700

    In class loops. Cars done. Weapons In progress
    Started to follow a long with in-class loops, did not finish weapons section will need to revisit.

---
+++ SimpleScenePractice/Assets/Scripts/Re-PracticeScripts/InClassLoops.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InClassLoops : MonoBehaviour
+{
+    public int number, total;
+
+    public string[] cars;
+
+    public Weapon weapons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            total = i;
+            print(total);
+        }
+
+        foreach (var car in cars)
+        {
+            print("What is your favorite type of" + car);
+        }
+
+        foreach (var weapons in Weapon)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
