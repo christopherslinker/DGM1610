c21725730b819b4fe72e38f98dcad31d31bba796
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-26 10:42:51 -0700

    Loop Behavior in class
    Loop Behavior script followed with class

---
+++ SimpleScenePractice/Assets/Scripts/LoopBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehaviour : MonoBehaviour
+{
+    public string[] playerNames;
+
+    private IEnumerator start()
+    {
+        foreach (var playerName in playerNames)
+        {
+            print(playerName);
+        }
+
+        while (true)
+        {
+            
+        }
+    }
+}
