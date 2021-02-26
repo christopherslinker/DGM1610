e334a256d142466011fafc7add5b91b9df894a58
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-26 11:08:10 -0700

    Weapon Collection Behavior
    In class weapons collection behavior I will have to bring back my weapons code I removed earlier because I was trying to keep my script folder clear.

---
+++ SimpleScenePractice/Assets/Scripts/WeaponsCollectionBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsCollectionBehavior : MonoBehaviour
+{
+    public Weapon weaponOBJ;
+
+    public WeaponCollection WeaopCollectionOBJ;
+
+    private void Start()
+    {
+        foreach (var weapon in WeaponCollectionOBJ.weapList)
+        {
+            if (weapon == weaponOBJ)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        WeaponCollectionOBJ.weaponList.Add(weaponOBJ);
+        gameObject.SetActive(false);
+    }
+}
