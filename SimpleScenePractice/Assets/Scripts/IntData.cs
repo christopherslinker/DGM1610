ae2550bb55c956b672a26b7d87cba76dbcf7caa3
Christopher Slinker <christopher.slinker@gmail.com>
2021-02-26 13:13:14 -0700

    Weapons Scriptable Object Added Back

--- SimpleScenePractice/Assets/Scripts/IntData.cs
+++ SimpleScenePractice/Assets/Scripts/IntData.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntData : MonoBehaviour
+public class IntData : ScriptableObject
 {
-    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
