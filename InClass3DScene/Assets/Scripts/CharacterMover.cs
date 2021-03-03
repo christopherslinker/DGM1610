39f6f5f5e729c9e0d229eef4f851a2cb154039aa
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-03 15:35:33 -0700

    Tried to add a jump button
    Tried to adda jump button but it does not work properly

--- InClass3DScene/Assets/Scripts/CharacterMover.cs
+++ InClass3DScene/Assets/Scripts/CharacterMover.cs
@@ -7,18 +7,22 @@ public class CharacterMover : MonoBehaviour
     public CharacterController controller;
     public Vector3 movement;
     public float speed = 3f, gravity = -8f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 yDirection;
 
-    // Update is called once per frame
+    public float jumpForce = 300f;
     void Update()
     {
-        movement.y = gravity;
-        movement.x = speed * Input.GetAxis("Horizontal");
-        movement.z = speed * Input.GetAxis("Vertical");
-        controller.Move(movement * Time.deltaTime);
+        {
+            movement.y = gravity;
+            movement.x = speed * Input.GetAxis("Horizontal");
+            movement.z = speed * Input.GetAxis("Vertical");
+            controller.Move(movement * Time.deltaTime);
+        }
+
+        /*if (Input.GetButtonDown("Jump"))
+        {
+            yDirection.y = jumpForce;
+            BoxCollider.AddForce(yDirection, ForceMode2D.Force);
+        }*/
     }
 }
