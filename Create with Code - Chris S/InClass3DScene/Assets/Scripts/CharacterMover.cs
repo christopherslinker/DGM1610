d0fc2fc6b974c141f158bf979acc84a93126297d
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-03 10:55:26 -0700

    In Class 3D file and Character Control C#
    I added a separate file for 3D in class work and added the character control script

---
+++ Create with Code - Chris S/InClass3DScene/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 movement;
+    public float speed = 3f, gravity = -8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.y = gravity;
+        movement.x = speed * Input.GetAxis("Horizontal");
+        controller.Move(movement * Time.deltaTime);
+    }
+}
