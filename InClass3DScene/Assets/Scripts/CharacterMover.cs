3d13eccc17590edc6e1ec83215a1eec2a43164e4
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-15 11:01:32 -0600

    InClass in game coding updated

--- InClass3DScene/Assets/Scripts/CharacterMover.cs
+++ InClass3DScene/Assets/Scripts/CharacterMover.cs
@@ -1,35 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMover : MonoBehaviour
 {
-    public CharacterController controller;
-    public Vector3 movement, rotation;
-    public float speed = 100f, gravity = -8f;
-    private Vector3 yDirection;
-    public GameObject projectilePrefab;
-    public float jumpForce = 300f;
+    public float speed = 3f, graivty = -8f, jumpForce = 10f;
+
+    private CharacterController controller;
+    private Vector3 movement, rotation;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
     void Update()
     {
-        {
-            movement.y = gravity;
-            movement.x = speed * Input.GetAxis("Horizontal");
-            movement.z = speed * Input.GetAxis("Vertical");
-            rotation.y = Input.GetAxis("Horizontal");
-            transform.Rotate(rotation);
-            movement = transform.TransformDirection(movement);
-            controller.Move(movement * Time.deltaTime);
-        }
+        movement.Set(speed*Input.GetAxis("Vertical"), graivty, 0);
+        rotation.y = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetButtonDown("Jump"))
         {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            movement.y = jumpForce;
         }
-        /*if (Input.GetButtonDown("Jump"))
-        {
-            yDirection.y = jumpForce;
-            BoxCollider.AddForce(yDirection, ForceMode2D.Force);
-        }*/
+        
+        transform.Rotate(rotation);
+        movement = transform.TransformDirection(movement);
+        controller.Move(movement * Time.deltaTime);
     }
 }
