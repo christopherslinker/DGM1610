0b3deb0943952177262ea8551f5810e927e94caf
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-16 16:24:24 -0600

    Started on adding an enemy Spawner.

---
+++ InClass3DScene/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefab;
+
+    private float spawnRangeX = 4;
+
+    private float spawnPosZ = -180;
+
+    private float startDelay = 2;
+
+    private float startInterval = 1.0f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, startInterval);
+    }
+
+    void Update()
+    {
+        //nothing here... for now.
+    }
+
+    void SpawnRandomEnemy()
+    {
+
+    }
+}
