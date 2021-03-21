53f3b7699865096eeb3362c47a6b1c910f2f4e8d
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-21 12:03:09 -0600

    Added Destroy Behaviour
    I added destroy behaviour to bulelts. It is helping with the data build up, but the bullets still don't work.

---
+++ InClass3DScene/Assets/Scripts/DestroyBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBehaviour : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
+
