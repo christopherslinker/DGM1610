c3bb25193cb66f9f04938f8ad93a1cb4b19978dc
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-23 21:43:57 -0600

    3.1 Finished

--- Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +8,24 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifier;
+    public bool isOnGround = true;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
---
+++ Create With Code/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclesPrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+
+    private float repeatDelay = 2;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclesPrefab, spawnPos, obstaclesPrefab.transform.rotation);
+    }
+}
