a36961f86d98ec6c90bac90de5bf653bbcccdb3b
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-26 22:13:38 -0600

    Lab3 My attempt player controls
    I attempted to make player controls

---
+++ Create With Code/Chris S. - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15f;
+    private float horizontalInput;
+    private float forwardInput;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        
+    }
+}
