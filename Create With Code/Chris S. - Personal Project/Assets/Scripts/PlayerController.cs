a79de203b377fb3007c522a501b2a26a3d75540a
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-26 22:25:10 -0600

    Lab3 Followed the tutorial

--- Create With Code/Chris S. - Personal Project/Assets/Scripts/PlayerController.cs
+++ Create With Code/Chris S. - Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 15f;
+    // my attempt
+   /* private float speed = 15f;
     private float horizontalInput;
-    private float forwardInput;
+    private float forwardInput;*/
+   
+   //tutorials scripts
+   private float speed = 10.0f;
+   private Rigidbody playerRb;
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        //my attempt
+        /*horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);*/
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+
     }
 }
