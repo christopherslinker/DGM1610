796827a1016e2fcab3c263cc689f403aa0974aab
Christopher Slinker <christopher.slinker@gmail.com>
2021-03-28 09:36:41 -0600

    Lab 3 finished

--- Create With Code/Chris S. - Personal Project/Assets/Scripts/PlayerController.cs
+++ Create With Code/Chris S. - Personal Project/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,9 @@ public class PlayerController : MonoBehaviour
    //tutorials scripts
    private float speed = 10.0f;
    private Rigidbody playerRb;
+   private float zBound = 6;
+   private float xBound = 15;
+   
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -27,12 +30,46 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);*/
 
+        MovePlayer();
+        ConstrainsPlayerPosition();
+
+
+
+
+
+    }
+    
+    //moves the player based on arrow key input
+    void MovePlayer()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+    
+    //Invisible walls
+    void ConstrainsPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
 
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
 
+        if (transform.position.x < -xBound)
+        {
+            transform.position = new Vector3(transform.position.z, transform.position.y, -xBound);
+        }
+
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(transform.position.z, transform.position.y, xBound);
+        }
     }
 }
