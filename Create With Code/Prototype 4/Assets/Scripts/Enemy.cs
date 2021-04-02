9416efd7689408b92d4f9b86585e91a0c496e237
Christopher Slinker <christopher.slinker@gmail.com>
2021-04-02 10:43:02 -0600

    4.4 finished
    I forgot to push this last night, just noticed it.

--- Create With Code/Prototype 4/Assets/Scripts/Enemy.cs
+++ Create With Code/Prototype 4/Assets/Scripts/Enemy.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
-
     private Rigidbody enemyRb;
-
     private GameObject player;
-    // Start is called before the first frame update
+
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
@@ -22,5 +20,11 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
--- Create With Code/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create With Code/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -1,17 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
-
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+    }
+    
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -21,4 +46,5 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
     }
+    
 }
