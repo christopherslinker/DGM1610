94bddd396093bd78cda71b70fdade79e4ca3fa7d
Christopher Slinker <christopher.slinker@gmail.com>
2021-04-02 19:15:51 -0600

    in class code

---
+++ InClass3DScene/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image img;
+    public FloatData dataObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.fillAmount = dataObj.value;
+    }
+}
