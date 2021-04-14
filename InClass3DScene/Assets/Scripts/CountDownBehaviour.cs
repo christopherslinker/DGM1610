719e8d3ecd04841a2d4ae755e9d5f1c3406dbd14
Christopher Slinker <christopher.slinker@gmail.com>
2021-04-14 13:41:23 -0600

    CountDownBehaviour from class

---
+++ InClass3DScene/Assets/Scripts/CountDownBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownBehaviour : MonoBehaviour
+{
+    public int number = 3;
+    public float holdTime = 1f;
+    public string endText = "GO!";
+
+    private Text countDownText;
+    private WaitForSeconds wfsObj;
+
+    private IEnumerator Start()
+    {
+        countDownText = GetComponent<Text>();
+        wfsObj = new WaitForSeconds(holdTime);
+
+        while (number > 0)
+        {
+            countDownText = number.ToString();
+            number--;
+            yield return wfsObj;
+        }
+
+        countDownText.text = endText;
+        yield return wfsObj;
+        countDownText.text = null;
+    }
+}
