7e90cf83d46b9f9a2d7222b4e90366993085a590
Christopher Slinker <christopher.slinker@gmail.com>
2021-04-25 22:07:09 -0600

    Menu and Start button
    trying to get a menu and start button working but I am struggling with it.

---
+++ Create With Code/Chris S. - Personal Project/Assets/Scripts/StartButtonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StartButtonBehaviour : MonoBehaviour
+{
+
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        
+        Debug.Log(button.gameObject.name + "was clicked!");
+    }
+}
