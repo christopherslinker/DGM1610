2d7e65df2985492c635aa8e225a4543009f9f4a7
Christopher Slinker <christopher.slinker@gmail.com>
2021-04-27 16:34:32 -0600

    Scriptable Object Coin

---
+++ SimpleScenePractice/Assets/Scripts/ScriptableObjects.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = " New Coin", menuName = "Coins")]
+public class ScriptableObjects : ScriptableObject
+{
+    public new string name;
+    
+    public int coinCost;
+
+}
